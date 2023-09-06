0f9efd5dabb9b81efdc6d6b29e32cd161684595a
pragya3421 <pragya3421@gmail.com>
2023-09-06 14:19:17 -0500

    Python practice done

--- Practice.py
+++ Practice.py
@@ -242,6 +242,137 @@ print(marks)
 
 #BREAK & CONTINUE - at 1:01:29
 
+#USING BREAK
+students = ["ram", "shyam", "kishan", "radha", "radhika"]
+
+for student in students:
+    if student == "radha":
+        break;
+    print(student)
+
+#USING CONTINUE
+
+for student in students:
+    if student == "kishan":
+        continue;
+    print(student)
+
+#TUPLE - Like List but it is immutable.
+#We can not modify tuple like list. We can not use insert, append, clear in tuble.
+#we can not assign items on any index.
+#Use parenthesis
+#It counts which object is how many times
+
+marks = (95, 98, 97, 97, 99)
+print(marks.count(97))
+
+# In tuple, there is no need to use parenthesis. We get the elements in tuple format only if we print. The print result will be in the format of tuple only.
+#Ex -
+person = "Jack", "Karen", "Connor", "Alanna"
+print(person)
+
+#see index of no.
+print(marks.index(95))
+
+#SETS
+#We use curly braces for set.
+#Since we do not have indexes in sets, it is called unordered.
+
+# #We need to store so many elements but want to keep only unique elements and not duplicates.
+# If there are repetition of element in set, only unique value and one element from duplicate will get printed.
+
+
+marks = {95, 98, 97, 97, 97}
+print(marks)
+
+#There is no existence of indexes in sets.
+marks = {95, 98, 97, 97, 97}
+print(marks[0])
+
+#We can not iterate over or access values using indexes in sets. We can only use loops to access the values or elements of sets.
+for score in marks:
+    print(score)
+
+
+##DICTIONARY
+#used to store key value pair
+#for single student with pair key information.
+
+#To access the single information, we write whole key in place of index number.
+#Key will be used as index.
+marks = {"english" : 95, "chemistry" : 98}
+print(marks["chemistry"])
+
+#Add new marks
+marks["physics"] = 97
+print(marks)
+
+#CHANGE MARKS
+marks["physics"] = 99
+print(marks)
+
+##FUNCTIONS
+#In-Built Functions
+#Module Functions
+#User-defined functions
+
+
+#In-Built Functions - int(), str(), bool()
+
+#Module Functions
+#Ex - Math module
+import math
+print(dir(math))
+
+from math import sqrt
+print(sqrt(4))
+
+from math import *  #* is used to get all the functions
+print(sqrt(4))
+
+#User-defined functions
+#Syntax below
+
+def function_name(parameters):
+    //do something
+#________________________________
+def print_sum(first, second):
+    print(first + second)
+
+print_sum(1, 2)
+
+#__________________________________________
+def print_sum(first, second=4):
+    print(first + second)
+
+
+print_sum(1)
+
+#_____________________________________
+def print_sum(first=3, second=4):
+    print(first + second)
+
+
+print_sum()
+#_______________________________________
+
+
+##########
+#Programming/Placements/Compatative Programming - Python (OOPS)
+#Machine Learning/Data Science - File Handling
+#Development - Django Framework
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
